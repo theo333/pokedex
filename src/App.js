b202c7c77baad56a8de336786fe6783cc12cf704
Theo Manton <theo333@gmail.com>
2020-07-18 12:24:39 -0400

    add get data from api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
+import Card from './components/Card';
+
 function App() {
+  const [currentPokemon, setCurrentPokemon] = useState({});
+  const [currentId, setCurrentId] = useState(1);
+
+  useEffect(() => {
+    getPokemon(1);
+  }, [currentId]);
+
+  const getPokemon = async (useId) => {
+    const response = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon/${useId}/`
+    );
+    const { id, height, weight } = response.data;
+    const { name } = response.data.forms[0];
+    const _current = {
+      id,
+      name,
+      height,
+      weight,
+    };
+    console.log('currentId: ', currentId);
+    console.log(_current);
+
+    setCurrentId(_current.id);
+    setCurrentPokemon(_current);
+  };
+
+  const { id, name, height, weight } = currentPokemon;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card
+        id={id}
+        name={name}
+        height={height}
+        weight={weight}
+      />
     </div>
   );
 }
