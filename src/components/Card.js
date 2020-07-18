9ceb5dd481fe3470ac2bc02c8c395fd570473020
Theo Manton <theo333@gmail.com>
2020-07-18 12:21:32 -0400

    add Card component

---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Card = ({ id, name, height, weight }) => {
+	return (
+		<>
+			<h1>name: {name}</h1>
+			<p>description</p>
+		</>
+	);
+};
+
+export default Card;
