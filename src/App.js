99b67f996280bf232e9d295a1d8c38afb41b8a63
Theo Manton <theo333@gmail.com>
2020-07-22 16:28:02 -0400

    add Router

--- src/App.js
+++ src/App.js
@@ -1,76 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { ChevronLeft, ChevronRight } from 'react-feather';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import Card from './components/Card';
 import Search from './components/Search';
 
-function App() {
-  const [currentPokemon, setCurrentPokemon] = useState({});
-  const [currentId, setCurrentId] = useState(1);
-
-  useEffect(() => {
-    getPokemon(currentId);
-  }, [currentId]);
-
-  // get Pokemon object from API
-  // used to display single Pokedex and for search
-  // pokemonIdentifier: ID (number) or name (string)
-  const getPokemon = async (pokemonIdentifier) => {
-    const singlePokemon = await axios.get(
-      `https://pokeapi.co/api/v2/pokemon/${pokemonIdentifier}/`
-    );
-    console.log('singlePokemon: ', singlePokemon.data);
-    const { id, height, weight } = singlePokemon.data;
-    const { name } = singlePokemon.data.forms[0];
-    const image = singlePokemon.data.sprites.front_default;
-    const _current = {
-      id,
-      name,
-      height,
-      weight,
-      image,
-    };
-    // console.log('currentId: ', currentId);
-    // console.log(_current);
-
-    setCurrentId(_current.id);
-    setCurrentPokemon(_current);
-  };
-
-  // change ID to previous or next Pokemon
-  const changePokemon = direction => {
-    // TODO add exceptions for first and last IDs
-    console.log('direction: ', direction, currentId);
-    const newId = direction === 'prev' ? currentId - 1 : currentId + 1;
-    console.log('newId: ', newId);
-    setCurrentId(newId);
-  };
-
-  const { id, name, weight, height, image } = currentPokemon;
+const App = () => {
   return (
-    <section className='App container'>
-      <h2>Pokedex</h2>
-      <Search />
-      <article className='main'>
-        <img className='img' src={image} /><br />
-        <div className='nav'>
-          <button className='btn-nav' onClick={() => changePokemon('prev')} disabled={currentId === 1 ? `{true}` : ''}>
-            <ChevronLeft color='red' size={90} />
-          </button>
-          <button className='btn-nav' onClick={() => changePokemon('next')} >
-            <ChevronRight color='red' size={90} />
-          </button>
-        </div>
-        <Card
-          id={id}
-          name={name}
-          weight={weight}
-          height={height}
-        />
-      </article>
-    </section>
+    <>
+      <Switch>
+        {/* TODO get ID match working */}
+        {/* <Route exact path='/:pokemonId' render={({ match }) => <Card pokemonId={match.params.pokemonId} />} /> */}
+        <Route exact path='/search' component={Search} />
+        <Route path='/' component={Card} />
+      </Switch>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { HashRouter as Router } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
 const root = document.getElementById('root');
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   root
 );
