2728630676951691e9a9082cdf55764abd86e60d
Theo Manton <theo333@gmail.com>
2020-07-23 09:23:17 -0400

    update Search with getPokemonByName

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,6 +2,23 @@ import React from 'react';
 import SearchForm from './SearchForm';
 
 const Search = () => {
+	const [currentPokemon, setCurrentPokemon] = useState({});
+	const [currentId, setCurrentId] = useState(1);
+
+	useEffect(() => {
+		getPokemonByName(currentId);
+	}, [currentId]); // 
+
+	// get single Pokemon object from API
+	// used to display single Pokedex from search
+	// pokemonName: name (string)
+	const getPokemonByName = async (pokemonName) => {
+		const searchedPokemon = await getSinglePokemon(pokemonName);
+
+		setCurrentId(searchedPokemon.id);
+		setCurrentPokemon(searchedPokemon);
+	};
+
 	return (
 		<section className='container'>
 			<p>Search</p>
