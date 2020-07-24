a6fc394a0423841fa9a7165ddbee0ebc23e78605
Theo Manton <theo333@gmail.com>
2020-07-24 02:38:06 -0400

    Updates

---
+++ precache-manifest.abc397440b53105288dfb403c94c9155.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4a0905bbb78462a701a38ba0ab5182c",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "3d316073ba6a49dea218",
+    "url": "/pokedex/static/css/main.a1cb468d.chunk.css"
+  },
+  {
+    "revision": "770966fcc5ec7e86a37f",
+    "url": "/pokedex/static/js/2.b65d8e62.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokedex/static/js/2.b65d8e62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d316073ba6a49dea218",
+    "url": "/pokedex/static/js/main.fe2d0ffe.chunk.js"
+  },
+  {
+    "revision": "5d5b7317cf43a0b8925c",
+    "url": "/pokedex/static/js/runtime-main.2d94522c.js"
+  }
+]);
