bcb9ac0e0a0581eae69de57e652da86cd4ef3f8f
Theo Manton <theo333@gmail.com>
2020-07-24 04:32:58 -0400

    remove comment from utils

--- src/utils.js
+++ src/utils.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 // gets Pokemon data from API
 // pokemonIdentifier: ID (integer) or name (string)
 const getSinglePokemon = async (pokemonIdentifier) => {
-	// TODO memoize data so that do not have to keep grabbing from API
 	const { data } = await axios.get(
 		`https://pokeapi.co/api/v2/pokemon/${pokemonIdentifier}/`
 	);
