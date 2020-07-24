17aa8b36e4ab9a42801503e5a33a63fe719b6b92
Theo Manton <theo333@gmail.com>
2020-07-24 04:08:59 -0400

    add Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+import Header from './components/Header';
 import Pokedex from './components/Pokedex';
 import Search from './components/Search';
 
 const App = () => {
   return (
     <main>
+      <Header />
       <section className='container'>
         < article className='main' >
           <Switch>
